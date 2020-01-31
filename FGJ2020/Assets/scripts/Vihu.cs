9d72dc5a7dbb1487a6ac65cdae35913e5939490d
Topias Koskela <r3bootyourmind@gmail.com>
2020-02-01 01:17:39 +0200

    Lisäilin vihujen scriptit ja niihin logiikkaa.

---
+++ FGJ2020/Assets/scripts/Vihu.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vihu : MonoBehaviour
+{
+    public int hp = 100;
+    public double speedScale = 1.0;
+    public double speedScaleWhenSlowed = 0.5;
+    public bool isSlowed = false;
+    public bool isImmuneToSlow = false;
+    public VihuWaypoint nykyinenKohde;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //tekee vahinkoa vihuun halutun maaran
+    void damage (int maara)
+    {
+        if (hp >= maara)
+        {
+            //hp:ta on enemman kuin mita on tulossa, niin otetaan vahinkoa
+            hp = hp - maara;
+            //TODO: paivita sprite jos tarvetta
+        }
+        else
+        {
+            hp = 0;
+            kuole;
+        }
+    }
+
+    //tuhoaa vihun ja kutsuu rajahdyksen
+    void kuole()
+    {
+        //TODO: implementoi kuolema ja rajahdys
+    }
+
+    // uusi kohde ja mahdollisesti beissin hajotus tai elamien vahennus
+    void paasiKohteeseen(VihuWaypoint kohde)
+    {
+        if (kohde.onkoViimeinen)
+        {
+            kuole;
+            //TODO vähennä elämiä pelaajalta
+        }
+        else
+        {
+            nykyinenKohde = kohde.seuraavaKohde;
+
+        }
+    }
+
+    //is called a fixed number of times per second
+    void FixedUpdate ()
+    {
+        //TODO liikuta vihua kohti waypointtia. A* vois toimia, mutta pitää tehdä kenttä sille
+    }
+}
---
+++ FGJ2020/Assets/scripts/VihuSpawner.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VihuSpawner : MonoBehaviour
+{
+    // Aallot määrittää kuinka monessa kasassa vihut tulee
+    public int aaltoja = 5;
+    public int valiaikaVihut = 10; // kuinka monen framin valein vihut tulevat
+    public int valiaikaAallot = 100; // kuinka monen framin valein aallot tulee
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //is called a fixed number of times per second
+    void FixedUpdate()
+    {
+        //TODO laske milloin pitää spawnaa vihuja ja milloin odotetaan aaltoja
+    }
+}
