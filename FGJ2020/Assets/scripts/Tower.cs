8622ae7cdb43ceb7e90f9affb004589b8d27417e
Joni Kiviluoto <joni.kiviluoto@gmail.com>
2020-02-01 16:46:09 +0200

    Tower now faces enemy once it enters onto given range

--- FGJ2020/Assets/scripts/Tower.cs
+++ FGJ2020/Assets/scripts/Tower.cs
@@ -5,17 +5,57 @@ using UnityEngine;
 public class Tower : MonoBehaviour
 {
     public int health;
+    public Transform target;
+    public float range = 2f;
+    public string enemyTag = "Enemy";
+    public Transform partToRotate;
+    public Rigidbody2D rb;
 
-    // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("I'm working");
-        AddHp(5);
+        InvokeRepeating("UpdateTarget", 0f, 0.5f);
+    }
+
+    void UpdateTarget()
+    {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag(enemyTag);
+        float shortestDistance = Mathf.Infinity;
+        GameObject nearestEnemy = null;
+
+        foreach (GameObject enemy in enemies)
+        {
+            float distanceToEnemy = Vector2.Distance(transform.position, enemy.transform.position);
+            if (distanceToEnemy < shortestDistance)
+            {
+                shortestDistance = distanceToEnemy;
+                nearestEnemy = enemy;
+                // Debug.Log("New enemy detected: " + nearestEnemy.name);
+            }
+
+        }
+
+        if (nearestEnemy != null && shortestDistance <= range)
+        {
+            target = nearestEnemy.transform;
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, range);
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (target == null)
+            return;
+
+        Vector2 dir = target.position - transform.position;
+        Quaternion lookRotation = Quaternion.LookRotation(dir);
+        Vector2 rotation = lookRotation.eulerAngles;
+        rb.rotation = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg - 90f;
     }
 
     void OnCOllisionEnter2D(Collision2D collision)
