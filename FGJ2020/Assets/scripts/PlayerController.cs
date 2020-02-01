365829bb5af7027387b1cba0873a8e1ec0916232
Tommi Haapanen <thaapane@student.jyu.fi>
2020-02-01 14:47:57 +0200

    Wobotti on animoitu Whiteboxeilla

--- FGJ2020/Assets/scripts/PlayerController.cs
+++ FGJ2020/Assets/scripts/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour
   public float moveSpeed = 5f;
   public float liftspeed = 7f;
   public Transform carryPosition;
+  public Animator animator;
   private Rigidbody2D rb;
   private Vector2 movement;
   private GameObject nostettava;
@@ -23,6 +24,10 @@ public class PlayerController : MonoBehaviour
     // x and y movement
     movement.x = Input.GetAxisRaw("Horizontal");
     movement.y = Input.GetAxisRaw("Vertical");
+
+    animator.SetFloat("Horizontal", movement.x);
+    animator.SetFloat("Vertical", movement.y);
+    animator.SetFloat("Speed", movement.sqrMagnitude);
 
     if (Input.GetButtonDown("Fire1"))
     {
