0d208b2532d610a4eb0a5626de50b90b25114e80
Tommi Haapanen <thaapane@student.jyu.fi>
2020-02-01 22:44:11 +0200

    Facing toimii taas

--- FGJ2020/Assets/scripts/PlayerController.cs
+++ FGJ2020/Assets/scripts/PlayerController.cs
@@ -4,82 +4,86 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-  public float moveSpeed = 5f;
-  public float liftspeed = 7f;
-  public Transform carryPosition;
-  public Animator animator;
-  private Rigidbody2D rb;
-  private Vector2 movement;
-  private Vector2 lastMove;
-  private GameObject nostettava;
-  private bool playerMoving = false;
-  private bool playerLifting = false;
+    public float moveSpeed = 5f;
+    public float liftspeed = 7f;
+    public Transform carryPosition;
+    public Animator animator;
+    private Rigidbody2D rb;
+    private Vector2 movement;
+    private Vector2 lastMove;
+    private GameObject nostettava;
+    private bool playerMoving = false;
+    private bool playerLifting = false;
 
-  void Awake()
-  {
-    rb = gameObject.GetComponent<Rigidbody2D>();
-  }
-
-  // Update is called once per frame
-  void Update()
-  {
-    playerMoving = false;
-
-    // x and y movement
-    movement.x = Input.GetAxisRaw("Horizontal");
-    movement.y = Input.GetAxisRaw("Vertical");    
-
-    if (movement.x != 0 || movement.y != 0){
-      playerMoving = true;
-      lastMove = new Vector2(movement.x, movement.y);
+    void Awake()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
     }
 
-    animator.SetFloat("MoveX", movement.x);
-    animator.SetFloat("MoveY", movement.y);
-    animator.SetBool("PlayerMoving", playerMoving);
-    animator.SetFloat("LastMoveX", lastMove.x);
-    animator.SetFloat("LastMoveY", lastMove.y);
-
-    if (Input.GetButtonDown("Fire1"))
+    // Update is called once per frame
+    void Update()
     {
-      if (!playerLifting)
-      {
+        playerMoving = false;
+
+        // x and y movement
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        if (movement.x != 0 || movement.y != 0)
         {
-          playerLifting = true;
+            playerMoving = true;
+            lastMove = new Vector2(movement.x, movement.y);
         }
-      }
-      else
-      {
-        playerLifting = false;
-      }
 
+        animator.SetFloat("MoveX", movement.x);
+        animator.SetFloat("MoveY", movement.y);
+        animator.SetBool("PlayerMoving", playerMoving);
+        animator.SetFloat("LastMoveX", lastMove.x);
+        animator.SetFloat("LastMoveY", lastMove.y);
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            if (nostettava != null)
+            {
+                if (!playerLifting)
+                {
+                    {
+                        playerLifting = true;
+                    }
+                }
+                else
+                {
+                    playerLifting = false;
+                }
+            }
+
+        }
+
+        if (playerLifting)
+        {
+            liftObject();
+        }
+
+        // Turning towards mouse position
+        // mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
     }
 
-    if (playerLifting)
+    void OnTriggerEnter2D(Collider2D other)
     {
-      liftObject();
+        if (other.gameObject.tag == "nostettava")
+        {
+            nostettava = other.gameObject;
+            Debug.Log(nostettava);
+        }
     }
-
-    // Turning towards mouse position
-    // mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
-  }
-
-  void OnTriggerEnter2D(Collider2D other)
-  {
-    if (other.gameObject.tag == "nostettava")
+    void FixedUpdate()
     {
-      nostettava = other.gameObject;
-      Debug.Log(nostettava);
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
     }
-  }
-  void FixedUpdate()
-  {
-    rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
-  }
 
-  public void liftObject()
-  {
-    float step = liftspeed * Time.deltaTime;
-    nostettava.transform.position = Vector2.MoveTowards(nostettava.transform.position, carryPosition.position, step);
-  }
+    public void liftObject()
+    {
+        float step = liftspeed * Time.deltaTime;
+        nostettava.transform.position = Vector2.MoveTowards(nostettava.transform.position, carryPosition.position, step);
+    }
 }
